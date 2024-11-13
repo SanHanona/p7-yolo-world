bcf7578e8b43ff2cdf794a846f01aa488c477efe
Jonas Thorhauge <jonas.thorhauge@gmail.com>
2024-11-13 12:50:00 +0100

    quick push so San can use the pc

--- ros_ws/install/vision/share/vision/launch/vision_launch.py
+++ ros_ws/install/vision/share/vision/launch/vision_launch.py
@@ -3,20 +3,20 @@ import launch_ros.actions
 
 def generate_launch_description():
     return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='vision',
-            executable='JohnsFunciton',
-            name='JohnsFunciton'),
+        # launch_ros.actions.Node(
+        #     package='vision',
+        #     executable='JohnsFunciton',
+        #     name='JohnsFunciton'),
         launch_ros.actions.Node(
             package='vision',
             executable='yolo11_subscriber',
             name='yolo11_subscriber'),
+        launch_ros.actions.Node(
+            package='vision',
+            executable='depth_subscriber',
+            name='depth_subscriber'),
         # launch_ros.actions.Node(
-        #     package='demo_nodes_cpp',
-        #     executable='talker',
-        #     name='talker'),
-        # launch_ros.actions.Node(
-        #     package='demo_nodes_cpp',
-        #     executable='listener',
-        #     name='listener'),
+        #     package='vision',
+        #     executable='image_subscriber',
+        #     name='image_subscriber')
   ])
